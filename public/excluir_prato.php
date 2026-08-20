d30dca02b92a840da2f964c97b5efdda65cc0764
brayananzini07 <brayan_anzini@estudante.sesisenai.org.br>
2026-08-20 08:47:08 -0300

    add- finalizei o excluir_prato.php

--- public/excluir_prato.php
+++ public/excluir_prato.php
@@ -1,1 +1,15 @@
 <?php
+include "conexao.php";
+
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+
+if ($id !== null && is_numeric ($id)) {
+
+    $sql = $conexao->prepare("DELETE FROM pratos WHERE id = ?");
+    $sql->bind_param("i", $id);
+    $sql->execute();
+    $sql->close();
+}
+header("Location: listar_pratos.php?msg=Prato excluído com sucesso!");
+exit;
+?>
